3f2dbd76ce29aba178140a607e77a154c939a530
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-05-01 22:10:00 +0100

    jinja

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test_route/', views.test_route, name='testroute'),
+    path('', views.test_route, name='index'),
     path('test_route1/<str:pk>', views.test_route1, name='param'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projects_list = [
+    {'id': 1,
+    'name':'Multi vendor ecommerce',
+    'description':'many vendors selling many things online for everyone to see and buy!'
+    },
+    {'id': 2,
+    'name':'Uber eats',
+    'description':'many restaurants selling many food online for everyone to order and eat!'
+    },
+    {'id': 3,
+    'name':'Online arcade',
+    'description':'many games online for everyone to see and play and have fun!'
+    },
+    {'id': 4,
+    'name':'Accountability post it notes',
+    'description':'many people posting notes for their goals and daily activities to work and grow with others!'
+    },
+]
+
+
 def test_route(request):
-    return render(request, 'projects/projects.html')
+    number = 10
+    context = {'msg':'General Kenobi', 'number':number, 'projects':projects_list}
+    return render(request, 'projects/projects.html', context)
 
 def test_route1(request, pk):
     return render(request, 'projects/test.html')
-
