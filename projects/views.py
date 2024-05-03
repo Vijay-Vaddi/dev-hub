03e7984e0283ea477af9279bb3780d3dbcc69f8a
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-05-03 21:12:24 +0100

    basic forms

--- projects/views.py
+++ projects/views.py
@@ -1,25 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
-
-projects_list = [
-    {'id': 1,
-    'name':'Multi vendor ecommerce',
-    'description':'many vendors selling many things online for everyone to see and buy!'
-    },
-    {'id': 2,
-    'name':'Uber eats',
-    'description':'many restaurants selling many food online for everyone to order and eat!'
-    },
-    {'id': 3,
-    'name':'Online arcade',
-    'description':'many games online for everyone to see and play and have fun!'
-    },
-    {'id': 4,
-    'name':'Accountability post it notes',
-    'description':'many people posting notes for their goals and daily activities to work and grow with others!'
-    },
-]
+from .forms import Create_project_form
 
 
 def projects(request):
@@ -35,3 +17,8 @@ def project(request, pk):
     context = {'project':project}
 
     return render(request, 'projects/single_project.html', context)
+
+def create_project(request):
+    form = Create_project_form()
+    context = {'form':form}
+    return render(request, 'projects/project_form.html', context)
