e13590f341de4b75d220d59156d8e86b7deb83ee
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-05-09 12:47:12 +0100

    small changes

--- projects/forms.py
+++ projects/forms.py
@@ -4,4 +4,11 @@ from .models import Project
 class Create_project_form(ModelForm):
     class Meta:
         model = Project
-        fields = '__all__'
+        # fields = '__all__'
+        exclude = ('vote_ratio','vote_total') #[]
+    
+    # to set a styling
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'style':'color:red'})
+
