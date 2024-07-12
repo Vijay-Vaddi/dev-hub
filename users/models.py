f594830b8415ad29bb7bffaf36b2c1085d7549e1
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-07-12 19:19:36 +0100

    user create/delete signals

--- users/models.py
+++ users/models.py
@@ -49,8 +49,24 @@ def profile_updated(sender, instance, created, **kwargs):
 
 
 @receiver(post_delete, sender=Profile)
-def user_deleted(sender, instance, **kwargs):
-    print('Deleting user...') 
+def profile_deleted(sender, instance, **kwargs):
+    print('Deleting user...')
+    user = instance.user 
+    user.delete()
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    print('New user detected, creating user profile...')
+    user = instance
+    profile = Profile.objects.create(
+        user = user,
+        username = user.username,
+        email = user.email,
+        name = user.first_name,
+    )
+
+    print('Done!!')
 
 # one way to use signals
 # post_save.connect(profile_updated, sender=Profile)
