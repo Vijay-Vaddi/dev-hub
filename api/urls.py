bf33af529313eca11b8507e699aa20a10eb3fbd9
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-08-25 13:42:48 +0100

    serializer models, and get projects

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
-    path('', views.get_routes, name='get_routes')
+    path('', views.get_routes, name='get_routes'),
+    path('projects/',views.get_projects, name='get_projects'),
+    path('projects/<str:pk>/',views.get_project, name='get_projects'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,11 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProjectSerializer
+from projects.models import Project
 
 # document like API to see all available endpoints
+@api_view(['GET'])
 def get_routes(request):
     routes = [
         {'GET':'/api/projects'},
@@ -11,4 +16,21 @@ def get_routes(request):
         {'POST':'/api/users/token/refresh'},
     ]
     
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def get_projects(request):
+    projects = Project.objects.all()
+    # to serialize many objects set many=True, False for single obj
+    serializer = ProjectSerializer(projects, many=True)
+    
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_project(request, pk):
+    project = Project.objects.get(id=pk)
+    # to serialize many objects set many=True, False for single obj
+    serializer = ProjectSerializer(project, many=False)
+    
+    return Response(serializer.data)
