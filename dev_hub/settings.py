40dcf29b5fa5ff0caa6428c0c3ba99d091f2b35a
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-09-08 19:04:10 +0100

    connected to postgres

--- dev_hub/settings.py
+++ dev_hub/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'dev_hub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devsearch',
+        'USER': 'postgres',
+        'PASSWORD': 'Hello!23',
+        'HOST':'localhost',
+        'PORT':'5432', 
     }
 }
 
@@ -203,4 +214,4 @@ SIMPLE_JWT = {
 
 # to allow requests from all domains/servers, making it open 
 CORS_ALLOW_ALL_ORIGINS = True
-
+ 
