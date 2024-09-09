b12418f786d08c6b2e31081a7f006053dd1b536f
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-09-09 21:08:58 +0100

    aws postres connection

--- dev_hub/settings.py
+++ dev_hub/settings.py
@@ -90,14 +90,15 @@ WSGI_APPLICATION = 'dev_hub.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+from keys import password
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'devsearch',
-        'USER': 'postgres',
-        'PASSWORD': 'Hello!23',
-        'HOST':'localhost',
+        'USER': 'vijayvaddi',
+        'PASSWORD': password,
+        'HOST':'database-1.cd8ws0qy6eru.eu-north-1.rds.amazonaws.com',
         'PORT':'5432', 
     }
 }
